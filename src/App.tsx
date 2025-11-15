c328bd3062893cef2f3d0b435797ff2975058ffb
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2025-11-15 19:56:57 +0545

    Add Redux for state management and implement user data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import type { AppDispatch, RootState } from "./store";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserData } from "./store/users/usersSlice";
+
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { users, loading, error } = useSelector(
+    (state: RootState) => state.users
+  );
+
+  console.log(users, loading, error);
+
+  useEffect(() => {
+    dispatch(fetchUserData({ page: 2, limit: 10 }));
+  }, [dispatch]);
+
   return <h2 className="text-center">Hello world</h2>;
 }
 
